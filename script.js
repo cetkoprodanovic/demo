d77dee0e72eb37ab65e3ca96cb447f4aca213508
Cetko Prodanovic <cetko@zendev.se>
2022-05-06 19:47:21 +0200

    js to zendev message

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML="Demo content changed!!!";
+    document.getElementById("demo").innerHTML="Demo content changed to ZenDev!!!";
 }
